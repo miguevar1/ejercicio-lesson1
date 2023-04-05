6745485f8a4be2aaf9e081805faf01d8497a2865
Admin <Admin@DESKTOP-PLPG44L>
2023-04-05 15:31:41 -0400

    Agregar archivos de proyecto.

---
+++ EjercicioLesson1/DataAccess/EjercicioDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EjercicioLesson1.Models.DataModels;
+
+namespace EjercicioLesson1.DataAccess
+{
+    public class EjercicioDBContext : DbContext
+    {
+        public EjercicioDBContext(DbContextOptions<EjercicioDBContext> options) : base (options)
+        {
+        }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Curso>? Curso { get; set; }
+
+    }
+}
---
+++ EjercicioLesson1/Models/DataModels/Curso.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EjercicioLesson1.Models.DataModels
+{
+    public class Curso : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required, StringLength(280)]
+        public string DescriptionCorta { get; set; } = string.Empty;
+        [Required]
+        public string Descriptionlarga { get; set; } = string.Empty;
+        [Required]
+        public string  PublicoObjetivo { get; set;} = string.Empty;
+        [Required]
+        public string Objetivo { get; set; } = string.Empty;
+        [Required]
+        public string Requisitos { get; set; } = string.Empty;
+        
+        public enum Nivel { bajo, intermedio, avanzado};
+    }
+}
